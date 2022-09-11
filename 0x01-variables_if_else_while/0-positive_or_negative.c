1b4a46588f0ddd3bd6971fd3b3fead7fe5fc5bf7
Young-Xaves <valentinedogwu@gmail.com>
2022-09-10 21:47:38 -0700

    first task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <random>
+#include <time.h>
+/* more headers goes there */
+/**
+ *main - Entry point
+ *Description: this will check for the negativity of a random genrated number
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					/* your code goes there */
+				if (n >= 0)
+				{
+					printf("%u is positive\n", n);
+				}
+				else if (n == 0)
+				{
+					printf("%u is zero\n", n);
+				}
+				else if (n <= 0)
+				{
+					printf("%u is negative\n", n);
+				}
+					return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+My C langauge practice repository with this directory focusing on the variables if, else and while loops
