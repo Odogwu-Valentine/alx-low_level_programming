ce399ccb5d244ba0921d26e86e4c85d4a58b9b44
Young-Xaves <valentinedogwu@gmail.com>
2022-09-11 18:14:44 -0700

    task 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,16 @@
 int main(void)
 {
 	int A;
+	int a;
 
-	for (A = 65; A <= 122; A++)
+	for (a = 97 ; a <= 122 ; a++)
 	{
-		putchar(A);
+		putchar(a);
 	}
+		for (A = 65 ; A <= 90 ; A++)
+		{
+			putchar(A);
+		}
 putchar('\n');
 return (0);
 }
