e3673789a17a4708794746c40e2c4c9105757a48
Young-Xaves <valentinedogwu@gmail.com>
2022-09-11 19:16:15 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the digitd in base 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	return (0);
+}
