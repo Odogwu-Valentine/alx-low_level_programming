bd072436dfb01f319f6ad0f94aebe960830e5997
Young-Xaves <valentinedogwu@gmail.com>
2022-09-11 17:22:48 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : playing the alphabet game
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int A;
+
+	for (A = 1 ; A <= 9 ; A++)
+	{
+		putchar (A);
+	}
+return (0);
+}
