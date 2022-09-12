e336d01f9102413962607cc3942feb424560f2a1
Young-Xaves <valentinedogwu@gmail.com>
2022-09-11 20:07:03 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints number in reverse
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ahpla;
+
+	for (ahpla = 122; ahpla >= 97; ahpla--)
+	{
+		putchar(ahpla);
+	}
+	putchar('\n');
+	return (0);
+}
