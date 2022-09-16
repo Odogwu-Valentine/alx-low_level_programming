072bc5b51758799525f566dc1adf2ebb0eac50d7
Young-Xaves <valentinedogwu@gmail.com>
2022-09-15 18:34:36 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - adds two numbers
  * int a is the first number to be added while int b, the second
  * @a: first number to add
- * @: second number to be aded
+ * @b: second number to be aded
  * Return: return th sum
  */
 int add(int a, int b)
