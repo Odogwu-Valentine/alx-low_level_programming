3af16ee71b8190f8951d58622d10d6b9c99714b7
Young-Xaves <valentinedogwu@gmail.com>
2022-09-15 20:14:45 -0700

    task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,21 +6,16 @@
  */
 int main(void)
 {
-	int inc;
+	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
 
-	unsigned long n1 = 0, n2 = 1, n3;
-
-	for (inc = 0; inc < 50; inc++)
+	while (n3 <= 4000000)
 	{
 		n3 = n1 + n2;
-		printf("%lu", n3);
 		n1 = n2;
 		n2 = n3;
-
-		if (inc == 49)
-			printf("\n");
-		else
-			printf(", ");
+		if ((n1 % 2) == 0)
+			sum += n1;
 	}
+	printf("%ld\n", sum);
 	return (0);
 }
