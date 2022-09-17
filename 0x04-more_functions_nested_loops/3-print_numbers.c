d9dcbb6b4fac359c244eaade1ea062bce969d6a0
Young-Xaves <valentinedogwu@gmail.com>
2022-09-16 17:41:26 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the number from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num >= 48 && num <= 57)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+	return (0);
+}
