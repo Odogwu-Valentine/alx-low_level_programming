e2a345f92ea56a253dbeee5d824267c400171adf
Young-Xaves <valentinedogwu@gmail.com>
2022-09-16 17:14:29 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if an argument is a digit
+ * @c: The argument ot be checked for
+ * Return: 1 if the check is successful
+ */
+
+int _isdigit(int c)
+{
+	if (c <= -1 || c >= 0 )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
