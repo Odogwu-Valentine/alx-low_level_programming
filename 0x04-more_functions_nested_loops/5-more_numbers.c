d8185c0a05c5fbe2b7cc6ebea7afe3b38bafd5b2
Young-Xaves <valentinedogwu@gmail.com>
2022-09-16 18:48:53 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times number 0 t0 14
+ *
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	if (i <= 10)
+	{
+		for (n == 0; n <= 14; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
