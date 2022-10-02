a05726c49cd09f3b10e8525a1f3bb70f9134cd30
Young-Xaves <valentinedogwu@gmail.com>
2022-10-02 06:21:56 -0700

    task 100

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int x);
 int is_prime(int n, int start);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/
